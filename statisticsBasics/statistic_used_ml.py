1279f9bd9c716d038ae7d82b882f6689e6d9f341
bberry <bberry@in.imshealth.com>
2019-08-22 00:57:48 +0530

    statistic_used_ml

---
+++ statisticsBasics/statistic_used_ml.py
@@ -0,0 +1,244 @@
+# -*- coding: utf-8 -*-
+#https://www.kaggle.com/kanncaa1/statistical-learning-tutorial-for-beginners
+"""
+Basic statistic for beginners
+> Histogram
+> Outliers
+> Box Plot
+> Summary Statistics
+> CDF
+> Effect size
+> Relationship Between Variables
+> Correlation
+> Covariance
+> Pearson Correlation
+> Spearman's Rank Correlation
+> Mean VS Median
+> Hypothesis Testing
+> Normal(Gaussian) Distribution and z-score
+"""
+
+#Import  libraries
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+#from pandas.plotting import plotting
+from scipy import stats
+
+import os
+
+print(os.listdir("_Data"))
+#Breast Cancer Wisconsin (Diagnostic) Data Set
+data = pd.read_csv("_Data/data.csv")
+data = data.drop(['Unnamed: 32','id'],axis = 1)
+
+#Explore the data
+data.head()
+data.shape
+data.columns
+"""
+# Histogram
+How many times each value appears in dataset. This description is called the distribution of variable
+Most common way to represent distribution of varible is histogram that is graph which shows frequency of each value.
+Frequency = number of times each value appears
+Example: [1,1,1,1,2,2,2]. Frequency of 1 is four and frequency of 2 is three.
+"""
+m = plt.hist(data[data["diagnosis"] == 'M'].radius_mean ,bins = 30,fc =(1,0,0,0.5),label= 'Malignant')
+b = plt.hist(data[data["diagnosis"] == 'B'].radius_mean ,bins = 30,fc =(0,1,0,0.5),label= 'Benign')
+plt.legend()
+plt.xlabel("Radius Mean values")
+plt.ylabel("Frequency")
+plt.title("Histogram of Radius Mean for Benign and Malignant Tumors")
+plt.show()
+
+freq_Malignant_radius_mean = m[0].max()
+index_freq_Malignant_radius_mean = list(m[0]).index(freq_Malignant_radius_mean)
+most_freq_Malignant_radius_mean  = m[1][index_freq_Malignant_radius_mean]
+print("Most frequent malignant radius is : ", most_freq_Malignant_radius_mean)
+
+"""
+Most frequent malignant radius is : 20.101999999999997
+From this graph you can see that radius mean of malignant tumors are bigger than radius mean of bening tumors mostly.
+The bening distribution (green in graph) is approcimately bell-shaped that is shape of normal distribution (gaussian distribution)
+Also you can find result like that most frequent malignant radius mean is
+"""
+"""
+Outliers
+While looking histogram as yok can see there are rare values in bening distribution (green in graph)
+There values can be errors or rare events.
+These errors and rare events can be called outliers.
+Calculating outliers:
+    first we need to calculate first quartile (Q1)(25%)
+    then find IQR(inter quartile range) = Q3-Q1
+    finally compute Q1 - 1.5IQR and Q3 + 1.5IQR
+    Anything outside this range is an outlier
+lets write the code for bening tumor distribution for feature radius mean
+""" 
+data_benign = data[data['diagnosis']=='B']
+data_malignant = data[data['diagnosis']=='M']
+desc = data_benign.radius_mean.describe()
+Q1 = desc[4]
+Q3 = desc[6]
+IQR = Q3-Q1
+lower_bound = Q1 - 1.5 * IQR
+upper_bound = Q3 + 1.5 * IQR
+print("Anything outside this range is an outlier : (",lower_bound,",",upper_bound,")")
+print("Outliers : ", data_benign[(data_benign.radius_mean < lower_bound) | (data_benign.radius_mean > upper_bound)].radius_mean.values)
+
+"""
+Box Plot
+You can see outliers also from box plots
+We found 3 outlier in bening radius mean and in box plot there are 3 outlier.
+"""
+melted_data = pd.melt(data,id_vars='diagnosis',value_vars = ['radius_mean','texture_mean'])
+plt.figure(figsize=(15,10))
+sns.boxplot(x='variable', y ='value', hue ='diagnosis',data = melted_data)
+plt.show()
+
+"""
+Summary Statistics
+    Mean
+    Variance: spread of distribution
+    Standart deviation square root of variance
+Lets look at summary statistics of bening tumor radiance mean
+"""
+print("Mean : ",data_benign.radius_mean.mean())
+print("Variance : ",data_benign.radius_mean.var())
+print("Standard Deviation : ",data_benign.radius_mean.std())
+print("Describe : ",data_benign.radius_mean.describe())
+
+"""
+CDF
+Cumulative distribution function is the probability that the variable takes a value less than or equal to x. P(X <= x)
+Lets explain in cdf graph of bening radiues mean
+in graph, what is P(12 < X)? The answer is 0.5. The probability that the variable takes a values less than or equal to 12(radius mean) is 0.5.
+You can plot cdf with two different method
+"""
+plt.hist(data_benign.radius_mean,bins=50,fc=(0,1,0,0.5),label='Bening',normed = True,cumulative = True)
+sorted_data = np.sort(data_benign.radius_mean)
+y = np.arange(len(sorted_data))/float(len(sorted_data)-1)
+plt.plot(sorted_data,y,color='red')
+plt.title('CDF of bening tumor radius mean')
+plt.show()
+
+"""
+Effect size
+One of the summary statistics.
+It describes size of an effect. It is simple way of quantifying the difference between two groups.
+In an other saying, effect size emphasises the size of the difference
+Use cohen effect size
+Cohen suggest that if d(effect size)= 0.2, it is small effect size, d = 0.5 medium effect size, d = 0.8 large effect size.
+lets compare size of the effect between bening radius mean and malignant radius mean
+Effect size is 2.2 that is too big and says that two groups are different from each other as we expect. Because our groups are bening radius mean and malignant radius mean that are different from each other
+"""
+mean_diff = data_malignant.radius_mean.mean() - data_benign.radius_mean.mean()
+var_bening = data_benign.radius_mean.var()
+var_malignant = data_malignant.radius_mean.var()
+var_pooled = (len(data_benign)*var_bening +len(data_malignant)*var_malignant ) / float(len(data_benign)+ len(data_malignant))
+effect_size = mean_diff/np.sqrt(var_pooled)
+print("Effect size: ",effect_size)
+
+"""
+Relationship Between Variables
+We can say that two variables are related with each other, if one of them gives information about others
+For example, price and distance. If you go long distance with taxi you will pay more. There fore we can say that price and distance are positively related with each other.
+Scatter Plot
+Simplest way to check relationship between two variables
+Lets look at relationship between radius mean and area mean
+In scatter plot you can see that when radius mean increases, area mean also increases. Therefore, they are positively correlated with each other.
+There is no correlation between area mean and fractal dimension se. Because when area mean changes, fractal dimension se is not affected by chance of area mean
+"""
+plt.figure(figsize = (15,10))
+sns.jointplot(data.radius_mean,data.area_mean,kind="regg")
+plt.show()
+
+# Also we can look relationship between more than 2 distribution
+sns.set(style = "white")
+df = data.loc[:,["radius_mean","area_mean","fractal_dimension_se"]]
+g = sns.PairGrid(df,diag_sharey = False,)
+g.map_lower(sns.kdeplot,cmap="Blues_d")
+g.map_upper(plt.scatter)
+g.map_diag(sns.kdeplot,lw =3)
+plt.show()
+
+"""
+Correlation
+Strength of the relationship between two variables
+Lets look at correlation between all features.
+
+conclusion
+Huge matrix that includes a lot of numbers
+The range of this numbers are -1 to 1.
+Meaning of 1 is two variable are positively correlated with each other like radius mean and area mean
+Meaning of zero is there is no correlation between variables like radius mean and fractal dimension se
+Meaning of -1 is two variables are negatively correlated with each other like radius mean and fractal dimension mean.Actually correlation between of them is not -1, it is -0.3 but the idea is that if sign of correlation is negative that means that there is negative correlation.
+"""
+f,ax=plt.subplots(figsize = (18,18))
+sns.heatmap(data.corr(),annot= True,linewidths=0.5,fmt = ".1f",ax=ax)
+plt.xticks(rotation=90)
+plt.yticks(rotation=0)
+plt.title('Correlation Map')
+plt.savefig('graph.png')
+plt.show()
+
+"""
+Covariance
+Covariance is measure of the tendency of two variables to vary together
+So covariance is maximized if two vectors are identical
+Covariance is zero if they are orthogonal.
+Covariance is negative if they point in opposite direction
+Lets look at covariance between radius mean and area mean. Then look at radius mean and fractal dimension se
+"""
+np.cov(data.radius_mean,data.area_mean)
+print("Covariance between radius mean and area mean: ",data.radius_mean.cov(data.area_mean))
+print("Covariance between radius mean and fractal dimension se: ",data.radius_mean.cov(data.fractal_dimension_se))
+
+"""
+Pearson Correlation
+Division of covariance by standart deviation of variables
+Lets look at pearson correlation between radius mean and area mean
+First lets use .corr() method that we used actually at correlation part. In correlation part we actually used pearson correlation :)
+p1 and p2 is the same. In p1 we use corr() method, in p2 we apply definition of pearson correlation (cov(A,B)/(std(A)*std(B)))
+As we expect pearson correlation between area_mean and area_mean is 1 that means that they are same distribution
+Also pearson correlation between area_mean and radius_mean is 0.98 that means that they are positively correlated with each other and relationship between of the is very high.
+To be more clear what we did at correlation part and pearson correlation part is same.
+"""
+p1 = data.loc[:,["area_mean","radius_mean"]].corr(method= "pearson")
+p2 = data.radius_mean.cov(data.area_mean)/(data.radius_mean.std()*data.area_mean.std())
+print('Pearson correlation: ')
+print(p1)
+print('Pearson correlation: ',p2)
+
+"""
+Spearman's Rank Correlation
+Pearson correlation works well if the relationship between variables are linear and variables are roughly normal. But it is not robust, if there are outliers
+To compute spearman's correlation we need to compute rank of each value
+
+conslusion
+Spearman's correlation is little higher than pearson correlation
+If relationship between distributions are non linear, spearman's correlation tends to better estimate the strength of relationship
+Pearson correlation can be affected by outliers. Spearman's correlation is more robust.
+"""
+ranked_data = data.rank()
+spearman_corr = ranked_data.loc[:,["area_mean","radius_mean"]].corr(method= "pearson")
+print("Spearman's correlation: ")
+print(spearman_corr)
+"""
+Mean VS Median
+Sometimes instead of mean we need to use median. I am going to explain why we need to use median with an example
+Lets think that there are 10 people who work in a company. Boss of the company will make raise in their salary if their mean of salary is smaller than 5
+"""
+salary = [1,4,3,2,5,4,2,3,1,500]
+print("Mean of salary: ",np.mean(salary))
+print("Median of salary: ",np.median(salary))
+"""
+Mean of salary is 52.5 so the boss thinks that oooo I gave a lot of salary to my employees. And do not makes raise in their salaries. However as you know this is not fair and 500(salary) is outlier for this salary list.
+Median avoids outliers
+Now median of the salary is 3 and it is less than 5 and employees will take raise in their sallaries and they are happy and this situation is fair
+"""
+
+
+
+
+   
